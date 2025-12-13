c601f997e4691fda605364bbe4e7e1149d0ddd2f
XIL(しーる) <120433484+xil-0828@users.noreply.github.com>
2025-12-13 21:08:10 +0900

    fix: callbackエラーの修正のためlogin pageにforcd-dynamic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,12 @@
-'use client'
-
+'use client';
+export const dynamic = "force-dynamic";
 import GoogleLoginButton from "../components/auth/GoogleLoginButton";
+
 export default function LoginPage() {
   return (
     <div style={{ padding: 40 }}>
-
       <GoogleLoginButton />
-
       <hr style={{ margin: "20px 0" }} />
     </div>
-  )
+  );
 }
