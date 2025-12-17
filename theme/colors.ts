628635f5b445522249e728638297985d9c40abe3
XIL(しーる) <120433484+xil-0828@users.noreply.github.com>
2025-12-17 19:28:29 +0900

    themeの設定

---
+++ theme/colors.ts
@@ -0,0 +1,24 @@
+// theme/colors.ts
+export const colors = {
+  brand: {
+    primary:   { value: "#9BCEF8" },
+    secondary: { value: "#F0F4FB" },
+  },
+
+  bg: {
+    base: { value: "#FFFFFF" },
+  },
+
+  text: {
+    primary:   { value: "#000000" },
+    secondary: { value: "#8F96A1" },
+  },
+
+  border: {
+    default: { value: "#D6DEEA" },
+  },
+
+  rating: {
+    star: { value: "#FFEE2D" },
+  },
+};
--- theme/index.ts
+++ theme/index.ts
@@ -1,32 +1,14 @@
+// theme/index.ts
 import { createSystem, defaultConfig } from "@chakra-ui/react";
+import { colors } from "./colors";
+import { textStyles } from "./typography";
 
 const theme = createSystem(defaultConfig, {
   theme: {
     tokens: {
-      colors: {
-        brand: {
-          primary:   { value: "#9BCEF8" },
-          secondary: { value: "#F0F4FB" },
-        },
-
-        bg: {
-          base: { value: "#FFFFFF" },
-        },
-
-        text: {
-          primary:   { value: "#000000" },
-          secondary: { value: "#8F96A1" },
-        },
-
-        border: {
-          default: { value: "#D6DEEA" },
-        },
-
-        rating: {
-          star: { value: "#FFEE2D" },
-        },
-      },
+      colors,
     },
+    textStyles,
   },
 });
 
